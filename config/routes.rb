76735ee58ca6710583c10077a42e35dda1907c39
Adam Berro <adamberro@gmail.com>
2016-05-28 13:49:06 -0700

    add index view

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root "tweets#index"
+
+  resources :tweets
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
